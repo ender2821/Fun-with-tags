b22a0e42b9a73c30b2ba37531333512a95a697bd
Joshua Jensen <ender2821@yahoo.com>
2018-08-29 12:46:49 -0500

    initial boilerplate and html and styles

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// establish variables
+
+
+
+// generate array
+
+// add input and add button
+
+// add user submitted items to array
+
+// update array in DOM
+
+// add remove button to tags
+
+// remove tags from DOM 
+
+// pesist data with local storage. 
