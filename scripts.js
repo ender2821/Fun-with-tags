bf496497c20b3d6c9d789f5f7aba8e1ec04e6477
Joshua Jensen <ender2821@yahoo.com>
2018-08-29 13:42:27 -0500

    added functionality to UI

--- scripts.js
+++ scripts.js
@@ -1,17 +1,41 @@
 // establish variables
 
-
+const button = document.getElementById('addTag');
+const input = document.getElementById('tagName');
+const tags = document.querySelector('.tags');
+const tagsData = [];
 
 // generate array
 
-// add input and add button
+function generateTags() {
+  if (tagsData === undefined || tagsData.length == 0) {
+    console.log(tags);
+    const html = '<p>Add Some Tags!<p>';
+    tags.innerHTML = html;
+  } else {
+    tags.innerHTML = 'some tags exist';
+    tagsData.sort();
+    
+  }
+}
 
 // add user submitted items to array
 
+function submit(e) {
+  tagValue = input.value;
+  tagsData.push(tagValue);
+  generateTags();
+}
+
+button.addEventListener('click', submit);
 // update array in DOM
 
 // add remove button to tags
 
 // remove tags from DOM 
+
+// run function
+
+generateTags();
 
 // pesist data with local storage. 
