e24b1a5be790f598f61dd8d04acae9f8d586aa1a
Joshua Jensen <ender2821@yahoo.com>
2018-08-29 16:54:55 -0500

    persisted data with local storage

--- scripts.js
+++ scripts.js
@@ -4,22 +4,21 @@ const addButton = document.getElementById('addTag');
 const deleteButton = document.querySelector('deleteTag')
 const input = document.getElementById('tagName');
 const tags = document.querySelector('.tags');
-const tagsData = [];
-
+let storedData = [];
 // render tags
-
-function generateTags() {
-  if (tagsData === undefined || tagsData.length == 0) {
+function generateTags() {  
+  if (storedData === undefined || storedData.length == 0) {
     console.log(tags);
     let html = '<p>Add Some Tags!<p>';
     tags.innerHTML = html;
   } else {
-    tagsData.sort();
+    storedData.sort();
     let html = '';
-    for (i = 0; i < tagsData.length; i++) { 
-      html += '<li class="tag">' + tagsData[i] + '' + "</li>";
+    for (i = 0; i < storedData.length; i++) { 
+      html += '<li class="tag">' + storedData[i] + '' + "</li>";
     }
     tags.innerHTML = html;
+    localStorage.setItem("storedData", JSON.stringify(storedData));
   }
 }
 
@@ -27,14 +26,13 @@ function generateTags() {
 
 function submitTag(e) {
   tagValue = input.value;
-  tagsData.push(tagValue);
+  storedData.push(tagValue);
   input.value = '';
   generateTags();
 }
 
 addButton.addEventListener('click', submitTag);
 input.addEventListener("keyup", function(event) {
-  event.preventDefault();
   if (event.keyCode === 13) {
     submitTag();
   }
@@ -49,17 +47,23 @@ function deleteTag(e) {
 
 document.querySelector('body').addEventListener('click', function(e) {
   if (event.target.tagName.toLowerCase() === 'li') {
-    console.log(event.target.innerHTML)
-    const index = tagsData.indexOf(event.target.innerHTML);
+    const index = storedData.indexOf(event.target.innerHTML);
     if (index !== -1) {
-        tagsData.splice(index, 1);
+        storedData.splice(index, 1);
     }
     generateTags();
   }
 });
 
-// run function
-
-generateTags();
-
 // persist data with local storage. 
+
+function locateData() {
+  let parsedData = JSON.parse(localStorage.getItem("storedData"))
+  if (parsedData != undefined && parsedData != null) {
+    storedData = parsedData
+  } 
+}
+// run js
+
+locateData();
+generateTags()
